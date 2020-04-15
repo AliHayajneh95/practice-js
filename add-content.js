a38e56e86802fc139cc0ae0fd57b2fbf97d49d69
AliHayajneh95 <Hayajnehali95@gmail.com>
2020-04-15 15:03:50 +0300

    my first funtion in javascript

--- add-content.js
+++ add-content.js
@@ -2,7 +2,8 @@ function calculateAge()
 {
     var x = prompt('enter your year of birth');
     y = new Date().getFullYear();
-    alert('your age is ', 'y' - 'x');
+    age = (x - y).toString;
+    alert('your age is age');
 
 }
 
