1a7caac50c68c0ca328953cf0843e27372320a6f
AliHayajneh95 <63533171+AliHayajneh95@users.noreply.github.com>
2020-04-15 15:05:49 +0300

    Update add-content.js

--- add-content.js
+++ add-content.js
@@ -3,7 +3,7 @@ function calculateAge()
     var x = prompt('enter your year of birth');
     y = new Date().getFullYear();
     age = (x - y).toString;
-    alert('your age is age');
+    alert('y');
 
 }
 
